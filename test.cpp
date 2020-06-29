762ba8ba5dad77f1649442a9ab712c196ffda4bd
Roanoke <rowingwolfe@gmail.com>
2020-06-29 11:41:43 -0400

    Initial, hello world + notes

--- test.cpp
+++ test.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std; 
 //Namespace change so I don't have to always write std::cout.
+//Just a simple scratchpad here, nothing too special.
 int main(){
     int a = 0, b = 1, c = 3, d = 4;
     cout << "Hello, world. >=3" << std::endl; //The classic.
