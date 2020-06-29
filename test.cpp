aa8d069100d8b32a42d58bfffa74e70d32ad2cb5
Roanoke <rowingwolfe@gmail.com>
2020-06-29 11:38:15 -0400

    Initial, hello world.

---
+++ test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std; 
+//Namespace change so I don't have to always write std::cout.
+int main(){
+    int a = 0, b = 1, c = 3, d = 4;
+    cout << "Hello, world. >=3" << std::endl; //The classic.
+    cout << a + b + c + d;
+}
